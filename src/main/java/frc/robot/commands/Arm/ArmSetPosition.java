b5614abf6b223a3e909b263791a005c0694c09a6
Neal Kotval <neal.kotval@gmail.com>
2025-03-04 20:08:12 -0500

    fixed git ignore

--- src/main/java/frc/robot/commands/Arm/ArmSetPosition.java
+++ src/main/java/frc/robot/commands/Arm/ArmSetPosition.java
@@ -1,43 +1,43 @@
-// package frc.robot.commands.Arm;
-// import edu.wpi.first.wpilibj2.command.Command;
-// import frc.robot.Constants;
-// import frc.robot.subsystems.*;
+package frc.robot.commands.Arm;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.subsystems.*;
 
 
-// public class ArmSetPosition extends Command {
-//   private final Elevator elevator;
-//   private final double targetTicks;
-//   private final Arm arm;
+public class ArmSetPosition extends Command {
+  private final Elevator elevator;
+  private final double targetTicks;
+  private final Arm arm;
 
-//   public ArmSetPosition(Elevator elevator, Arm arm, double targetTicks) {
-//     this.elevator = elevator;
-//     this.arm = arm;
-//     this.targetTicks = targetTicks;
-//     addRequirements(elevator);
-//   }
+  public ArmSetPosition(Elevator elevator, Arm arm, double targetTicks) {
+    this.elevator = elevator;
+    this.arm = arm;
+    this.targetTicks = targetTicks;
+    addRequirements(elevator);
+  }
 
 
-//   @Override
-//   public void initialize() {
+  @Override
+  public void initialize() {
     
-//   }
+  }
 
-//   @Override
-//   public void execute() {
-//     if (arm.getTicks() < Constants.TickValues.armSafetyTicks) {
-//       arm.setArmPositionTicks(Constants.TickValues.armSafetyTicks);
-//     }
-//     elevator.setElevatorPositionTicks(targetTicks);
-//   }
+  @Override
+  public void execute() {
+    if (arm.getTicks() < Constants.TickValues.armSafetyTicks) {
+      arm.setArmPositionTicks(Constants.TickValues.armSafetyTicks);
+    }
+    elevator.setElevatorPositionTicks(targetTicks);
+  }
 
-//   @Override
-//   public boolean isFinished() {
-//     return Math.abs(elevator.getTicks()-targetTicks)<0.2;
-//   }
+  @Override
+  public boolean isFinished() {
+    return Math.abs(elevator.getTicks()-targetTicks)<0.2;
+  }
 
-//   @Override
-//   public void end(boolean interrupted) {
-//     double currentTicks = elevator.getTicks();
-//     elevator.setElevatorPositionTicks(currentTicks);
-//   }
-// }
+  @Override
+  public void end(boolean interrupted) {
+    double currentTicks = elevator.getTicks();
+    elevator.setElevatorPositionTicks(currentTicks);
+  }
+}
